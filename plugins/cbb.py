317e03b523e85d976d728e83ad1374809ab63a95
Godfatherpy <fschool749@gmail.com>
2024-09-22 22:04:04 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -7,7 +7,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = "<b>○ Hanielx : <a href='https://t.me/hanielxbackup'>",
+            text = "<b>○ Hanielx : <a href='https://t.me/hanielxbackup'",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
